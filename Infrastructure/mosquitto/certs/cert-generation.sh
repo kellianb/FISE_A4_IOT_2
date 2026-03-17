bc66e46cdec9c9d6c2d71bd8873e02d24b5eda66
FlorianLantz <florian.lantz@viacesi.fr>
2026-03-17 11:19:28 +0100

    Initial import

---
+++ Infrastructure/mosquitto/certs/cert-generation.sh
@@ -0,0 +1,17 @@
+# Generate CA (Certificate Authority) key and certificate
+openssl genrsa -out ca.key 2048
+openssl req -new -x509 -days 3650 -key ca.key -out ca.crt -subj "/CN=MyMQTT-CA"
+
+# Generate server key
+openssl genrsa -out server.key 2048
+
+# Generate server certificate signing request
+# IMPORTANT: Replace "192.168.1.100" with your actual broker IP address
+openssl req -new -key server.key -out server.csr -subj "/CN=192.168.137.1"
+
+# Sign the server certificate with CA
+openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 3650
+
+# Set proper permissions
+chmod 644 ca.crt server.crt
+chmod 600 ca.key server.key
